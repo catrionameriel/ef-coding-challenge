8e1763c8f684a2d756303d04fc7d9234b956ea0c
Catriona Meriel <catriona@1partcarbon.co.uk>
2019-07-28 16:50:31 +0100

    Set up words filters and count
    Create functions that remove the small words and a blacklist
    of unneeded words
    Count the remaining words

--- src/WordCloud.js
+++ src/WordCloud.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
 import ReviewData from './data/reviews.json'
+import Blacklist from './data/blacklist.json'
+
+// Review = Array of strings
+// Join all reviews together
+// Remove all annoying characters
+// Make all lowercase
+// make each word a seperate array
+// Filter out two letter words
+// Filter out words from blacklist
+// Create WordCloud object
+// Count words
 
 class WordCloud extends Component {
 
-  showReviews = (reviews) => {
-    return reviews.map((review, index) => <li key={index}>{review}</li>);
+  render() {
+    const wordList = this.setup(ReviewData.reviews);
+    console.log(wordList);
+    return (
+      <div>
+        Word Cloud
+      </div>
+      )
   }
 
-  render() {
-    return (
-      <ul>
-        { this.showReviews(ReviewData.reviews) }
-      </ul>
-    )
+  setup = (reviews) => {
+    const seperatedWords = this.createStringArrayFrom(reviews);
+    const filteredOutSmallWordsList = this.filterOutSmallWordsFrom(seperatedWords);
+    const filteredOutBlackListWords = this.filterOutBlacklistWordsFrom(filteredOutSmallWordsList, Blacklist.blacklist);
+    return this.countWords(filteredOutBlackListWords);
+  }
+
+  createStringArrayFrom = (array) => {
+    const string = this.removePunctuationFrom(array.join());
+    return string.split(' ');
+  }
+
+  removePunctuationFrom = (string) => {
+    const newString = string.replace(/[.,/!$%&*;:'-()0-9]/g, ' ');
+    return newString.toLowerCase();
+  }
+
+  filterOutSmallWordsFrom = (array) => {
+    return array.filter(word => word.length > 2)
+  }
+
+  filterOutBlacklistWordsFrom = (array, blacklist) => {
+     array = array.filter((word) => {
+      return !blacklist.includes(word);
+    });
+    return array;
+  }
+
+  countWords = (array) => {
+    const list = {};
+    array.forEach((word) => {
+      if (!list[word]) {
+        list[word] = 1;
+      } else {
+        list[word] += 1;
+      }
+    });
+    return list;
   }
 }
 
